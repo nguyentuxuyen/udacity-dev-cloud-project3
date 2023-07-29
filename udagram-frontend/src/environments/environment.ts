26dedca62557ff2cdd4a6783e5a18647a6250929
nhi.dai <nhimd2010@gmail.com>
2023-07-29 23:23:59 +0900

    fix external IP for frontend image

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a65d5df66a04f48d29255c479b3258e3-1998409462.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
